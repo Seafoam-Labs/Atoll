8b7f426ab05661b5759caff20fbcac0417a91c0f
Anton Ždanov <anton@azdanov.dev>
2026-08-22 20:07:53 +0300

    Enable major version upgrades and immediate application for DocumentDB cluster

--- terraform/docdb.tf
+++ terraform/docdb.tf
@@ -38,17 +38,19 @@ resource "aws_security_group" "docdb_sg" {
 }
 
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "${var.project_name}-docdb"
-  engine                  = "docdb"
-  engine_version          = var.docdb_engine_version
-  master_username         = var.docdb_master_username
-  master_password         = random_password.docdb_master.result
-  db_subnet_group_name    = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids  = [aws_security_group.docdb_sg.id]
-  storage_encrypted       = true
-  backup_retention_period = 7
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
+  cluster_identifier          = "${var.project_name}-docdb"
+  engine                      = "docdb"
+  engine_version              = var.docdb_engine_version
+  master_username             = var.docdb_master_username
+  master_password             = random_password.docdb_master.result
+  db_subnet_group_name        = aws_docdb_subnet_group.main.name
+  vpc_security_group_ids      = [aws_security_group.docdb_sg.id]
+  storage_encrypted           = true
+  backup_retention_period     = 7
+  preferred_backup_window     = "07:00-09:00"
+  skip_final_snapshot         = true
+  allow_major_version_upgrade = true
+  apply_immediately           = true
 }
 
 resource "aws_docdb_cluster_instance" "main" {
